f2ceb622adb70cecf0650f2c45c7d7968343968d
Janahan10 <janahan.ravichandran@gmail.com>
2022-08-25 14:30:45 +0000

    connected stock page to backend and render data

--- src/app/components/stock-page/stock-page.component.ts
+++ src/app/components/stock-page/stock-page.component.ts
@@ -1,15 +1,40 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { GetDataService } from 'src/app/services/get-data.service';
+
+interface Quote {
+  change: number,
+  changePercent: number
+  error: string,
+  high: number,
+  data: string,
+  low: number,
+  open: number,
+  prevClose: number,
+  price: number,
+  symbol: string,
+  volume: number
+}
 
 @Component({
   selector: 'app-stock-page',
   templateUrl: './stock-page.component.html',
   styleUrls: ['./stock-page.component.css']
 })
-export class StockPageComponent implements OnInit {
 
-  constructor() { }
+export class StockPageComponent implements OnInit {
+  quoteData
+  constructor(private http:HttpClient) { }
 
   ngOnInit() {
+    this.getQuoteData();
+  }
+
+  getQuoteData() {
+    const getData = new GetDataService(this.http);
+    getData.getStockQuote("MSFT").subscribe(data => {
+      this.quoteData = data
+    })
   }
 
 }
